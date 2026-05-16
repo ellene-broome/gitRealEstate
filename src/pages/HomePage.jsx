c483c0c33c501c9455fa3077e2118efd4d93199a
erbroome <ellenehall@gmail.com>
2026-05-16 14:01:34 -0500

    Add listing search filters

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,12 @@
 import ListingCard from "../components/ListingCard";
 import { listings } from "../data/listings";
 import Navbar from "../components/Navbar";
+import { useState } from "react";
 
 function HomePage() {
+    const [cityFilter, setCityFilter] = useState("");
+
+    const [bedsFilter, setBedsFilter] = useState("");
   return (
     <>
     <Navbar />
@@ -18,14 +22,48 @@ function HomePage() {
           </div>
         </div>
       </section>
+    <section className="filters-section">
+  <h2>Search Listings</h2>
+
+        <div className="filters">
+    <input
+      type="text"
+      placeholder="Filter by city..."
+      value={cityFilter}
+      onChange={(event) =>
+        setCityFilter(event.target.value)
+      }
+        />
+
+     <input
+      type="number"
+      placeholder="Minimum beds"
+      value={bedsFilter}
+      onChange={(event) =>
+        setBedsFilter(event.target.value)
+      }
+    />
+        </div>
+    </section>
 
       <section id="listings" className="listings-section">
         <h2>Featured Listings</h2>
 
         <div className="listing-grid">
-          {listings.map((listing) => (
-            <ListingCard key={listing.id} listing={listing} />
-          ))}
+          {listings
+    .filter((listing) => {
+        const matchesCity = listing.city
+      .toLowerCase()
+      .includes(cityFilter.toLowerCase());
+
+        const matchesBeds =
+      bedsFilter === "" || listing.beds >= Number(bedsFilter);
+
+        return matchesCity && matchesBeds;
+  })
+      .map((listing) => (
+    <ListingCard key={listing.id} listing={listing} />
+  ))}
         </div>
       </section>
     </>
