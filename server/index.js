dca1bb2a1d6fd8c14a3f4efa25b9839bae920268
erbroome <ellenehall@gmail.com>
2026-06-10 16:59:26 -0500

    Prepare backend CORS for deployment

--- server/index.js
+++ server/index.js
@@ -13,7 +13,22 @@ const supabase = createClient(
 );
 
 // Middleware
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.CLIENT_URL,
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 app.use(express.json());
 
 // Test route
